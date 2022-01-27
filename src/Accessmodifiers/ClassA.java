d8a2491aa54f9e5f900ec75b06436fcbc38d0a15
saikumar <saikumar.ksheerasagar@infor.com>
2022-01-27 13:30:15 +0530

    modified classA.java with variable

--- src/Accessmodifiers/ClassA.java
+++ src/Accessmodifiers/ClassA.java
@@ -11,10 +11,13 @@ public  class ClassA {
 	// private void display() {(private for private2 class)
   public void display() {
 		
-		int no =10;
+		int number =10;
 		
-		System.out.println("java acess modifiers private the int number is "+no);
+		System.out.println("java acess modifiers private the int number is "+number);
    }
+  
+  
+  
    
    
 	
